96bfcb75479a968fa2e2d81bcf1ed39d4ced59c8
Amruta <amrutatech@gmail.com>
2022-09-23 16:06:42 +0530

    implemented Authentication using jpa mysql db

--- spring-security-jdbc/src/main/java/org/practice/springsecurity/springSecurityWithJDBCH2.java
+++ spring-security-jdbc/src/main/java/org/practice/springsecurity/springSecurityWithJDBCH2.java
@@ -1,4 +1,4 @@
-    package org.practice.springsecurity;
+package org.practice.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -15,58 +15,58 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-    @EnableWebSecurity
-    public class springSecurityWithJDBCH2 {
+@EnableWebSecurity
+public class springSecurityWithJDBCH2 {
 
-        @Bean
-        public DataSource dataSource() {
-            return new EmbeddedDatabaseBuilder()
-                    .setType(EmbeddedDatabaseType.H2)
-                    .addScript("sql/users.sql")
-                    .build();
-        }
-
-        @Bean
-        public UserDetailsManager users() {
-
-            UserDetails user = User.builder()
-                    .username("user")
-                    .password("***")
-                    .roles("USER")
-                    .build();
-            UserDetails admin = User.builder()
-                    .username("admin")
-                    .password("@@@")
-                    .roles("USER", "ADMIN")
-                    .build();
-
-            JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource());
-            users.createUser(user);
-            users.createUser(admin);
-
-            return users;
-        }
-
-        @Bean
-        SecurityFilterChain webAuth(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests(authorize -> {
-                try {
-                    authorize
-                            .mvcMatchers("/admin").hasRole("ADMIN")
-                            .mvcMatchers("/user").hasRole("USER")
-                            .mvcMatchers("/").permitAll()
-                            .and().formLogin();
-                } catch (Exception e) {
-                    System.out.println("Exception occurred");
-                }
-            });
-            return http.build();
-        }
-
-        @Bean
-        public PasswordEncoder getPasswordEncoder() {
-            return NoOpPasswordEncoder.getInstance();
-        }
-
-
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("sql/users.sql")
+                .build();
     }
+
+    @Bean
+    public UserDetailsManager users() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("***")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("@@@")
+                .roles("USER", "ADMIN")
+                .build();
+
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource());
+        users.createUser(user);
+        users.createUser(admin);
+
+        return users;
+    }
+
+    @Bean
+    SecurityFilterChain webAuth(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> {
+            try {
+                authorize
+                        .mvcMatchers("/admin").hasRole("ADMIN")
+                        .mvcMatchers("/user").hasRole("USER")
+                        .mvcMatchers("/").permitAll()
+                        .and().formLogin();
+            } catch (Exception e) {
+                System.out.println("Exception occurred");
+            }
+        });
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+}
